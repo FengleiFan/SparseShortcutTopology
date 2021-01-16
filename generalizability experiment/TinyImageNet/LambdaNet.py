b29e3e3c7f9b60dd20b280f3cd4275d70904acc8
FengleiFan <hitfanfenglei@gmail.com>
2021-01-16 11:31:18 -0500

    tinyimagenet

---
+++ generalizability experiment/TinyImageNet/LambdaNet.py
@@ -0,0 +1,244 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class LambdaConv(nn.Module):
+    def __init__(self, in_channels, out_channels, heads=4, k=16, u=1, m=23):
+        super(LambdaConv, self).__init__()
+        self.kk, self.uu, self.vv, self.mm, self.heads = k, u, out_channels // heads, m, heads
+        self.local_context = True if m > 0 else False
+        self.padding = (m - 1) // 2
+
+        self.queries = nn.Sequential(
+            nn.Conv2d(in_channels, k * heads, kernel_size=1, bias=False),
+            nn.BatchNorm2d(k * heads)
+        )
+        self.keys = nn.Sequential(
+            nn.Conv2d(in_channels, k * u, kernel_size=1, bias=False),
+            nn.BatchNorm2d(k * u),
+        )
+        self.values = nn.Conv2d(in_channels, self.vv * u, kernel_size=1, bias=False)
+
+        self.softmax = nn.Softmax(dim=-1)
+
+        if self.local_context:
+            self.embedding = nn.Parameter(torch.randn([self.kk, self.uu, 1, m, m]), requires_grad=True)
+        else:
+            self.embedding = nn.Parameter(torch.randn([self.kk, self.uu]), requires_grad=True)
+
+    def forward(self, x):
+        n_batch, C, w, h = x.size()
+
+        queries = self.queries(x).view(n_batch, self.heads, self.kk, w * h) # b, heads, k // heads, w * h
+        softmax = self.softmax(self.keys(x).view(n_batch, self.kk, self.uu, w * h)) # b, k, uu, w * h
+        values = self.values(x).view(n_batch, self.vv, self.uu, w * h) # b, v, uu, w * h
+
+        lambda_c = torch.einsum('bkum,bvum->bkv', softmax, values)
+        y_c = torch.einsum('bhkn,bkv->bhvn', queries, lambda_c)
+
+        if self.local_context:
+            values = values.view(n_batch, self.uu, -1, w, h)
+            lambda_p = F.conv3d(values, self.embedding, padding=(0, self.padding, self.padding))
+            lambda_p = lambda_p.view(n_batch, self.kk, self.vv, w * h)
+            y_p = torch.einsum('bhkn,bkvn->bhvn', queries, lambda_p)
+        else:
+            lambda_p = torch.einsum('ku,bvun->bkvn', self.embedding, values)
+            y_p = torch.einsum('bhkn,bkvn->bhvn', queries, lambda_p)
+
+        out = y_c + y_p
+        out = out.contiguous().view(n_batch, -1, w, h)
+
+        return out
+
+
+class LambdaBottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(LambdaBottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+
+        self.conv2 = nn.ModuleList([LambdaConv(planes, planes)])
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.conv2.append(nn.AvgPool2d(kernel_size=(3, 3), stride=stride, padding=(1, 1)))
+        self.conv2.append(nn.BatchNorm2d(planes))
+        self.conv2.append(nn.ReLU())
+        self.conv2 = nn.Sequential(*self.conv2)
+
+        self.conv3 = nn.Conv2d(planes, self.expansion * planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion * planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.conv2(out)
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion * planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion * planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+# reference
+# https://github.com/kuangliu/pytorch-cifar/blob/master/models/resnet.py
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=1000):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        # ImageNet 350 epochs training setup
+        # self.maxpool = nn.Sequential(
+        #     nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1, bias=False),
+        #     nn.BatchNorm2d(64),
+        #     nn.ReLU()
+        # )
+
+        self.layer1 = self._make_layer(block, 64, num_blocks[0])
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Sequential(
+            nn.Dropout(0.3), # All architecture deeper than ResNet-200 dropout_rate: 0.2
+            nn.Linear(512 * block.expansion, num_classes)
+        )
+
+    def _make_layer(self, block, planes, num_blocks, stride=1):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for idx, stride in enumerate(strides):
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.maxpool(out)
+
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+
+        out = self.avgpool(out)
+        out = torch.flatten(out, 1)
+        out = self.fc(out)
+        return out
+
+
+def LambdaResNet18():
+    return ResNet(LambdaBottleneck, [2, 2, 2, 2])
+
+
+def LambdaResNet50():
+    return ResNet(LambdaBottleneck, [3, 4, 6, 3])
+
+
+def LambdaResNet152():
+    return ResNet(LambdaBottleneck, [3, 8, 36, 3])
+
+
+def LambdaResNet200():
+    return ResNet(LambdaBottleneck, [3, 24, 36, 3])
+
+
+def LambdaResNet270():
+    return ResNet(LambdaBottleneck, [4, 29, 53, 4])
+
+
+def LambdaResNet350():
+    return ResNet(LambdaBottleneck, [4, 36, 72, 4])
+
+
+def LambdaResNet420():
+    return ResNet(LambdaBottleneck, [4, 44, 87, 4])
+
+
+def ResNet50():
+    return ResNet(Bottleneck, [3, 4, 6, 3])
+
+
+def ResNet18():
+    return ResNet(Bottleneck, [2, 2, 2, 2])
+
+
+# reference
+# https://discuss.pytorch.org/t/how-do-i-check-the-number-of-parameters-of-a-model/4325
+def get_n_params(model):
+    pp=0
+    for p in list(model.parameters()):
+        nn=1
+        for s in list(p.size()):
+            nn = nn*s
+        pp += nn
+    return pp
+
+
+def check_params():
+    model = ResNet18()
+    print('ResNet18 baseline: ', get_n_params(model))
+
+    model = ResNet50()
+    print('ResNet50 baseline: ', get_n_params(model))
+
+    model = LambdaResNet50()
+    print('LambdaResNet50: ', get_n_params(model))
+
+    model = LambdaResNet152()
+    print('LambdaResNet152: ', get_n_params(model))
+
+    model = LambdaResNet200()
+    print('LambdaResNet200: ', get_n_params(model))
+
+    model = LambdaResNet270()
+    print('LambdaResNet270: ', get_n_params(model))
+
+    model = LambdaResNet350()
+    print('LambdaResNet350: ', get_n_params(model))
+
+    model = LambdaResNet420()
+    print('LambdaResNet420: ', get_n_params(model))
+
+
+# check_params()
---
+++ generalizability experiment/TinyImageNet/Tiny_RandomlyWired.py
@@ -0,0 +1,303 @@
+import torch, os
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.datasets as datasets
+import torch.utils.data as data
+import torchvision.transforms as transforms
+import torchvision.models as models
+import time, copy, sys, os
+
+
+from PIL import Image
+
+from RandomWiredModel import CNN
+import yaml
+
+
+
+
+os.environ["CUDA_VISIBLE_DEVICES"]="0,1,2"
+
+def parseClasses(file):
+    classes = []
+    filenames = []
+    with open(file) as f:
+        lines = f.readlines()
+    lines = [x.strip() for x in lines]
+    for x in range(0,len(lines)):
+        tokens = lines[x].split()
+        classes.append(tokens[1])
+        filenames.append(tokens[0])
+    return filenames,classes
+
+def load_allimages(dir):
+    images = []
+    if not os.path.isdir(dir):
+        sys.exit(-1)
+    for root, _, fnames in sorted(os.walk(dir)):
+        for fname in sorted(fnames):
+            #if datasets.folder.is_image_file(fname):
+            if datasets.folder.has_file_allowed_extension(fname,['.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif']):
+                path = os.path.join(root, fname)
+                item = path
+                images.append(item)
+    return images
+
+class TImgNetDataset(data.Dataset):
+    """Dataset wrapping images and ground truths."""
+    
+    def __init__(self, img_path, gt_path, class_to_idx=None, transform=None):
+        self.img_path = img_path
+        self.transform = transform
+        self.gt_path = gt_path
+        self.class_to_idx = class_to_idx
+        self.classidx = []
+        self.imgs, self.classnames = parseClasses(gt_path)
+        for classname in self.classnames:
+            self.classidx.append(self.class_to_idx[classname])
+
+    def __getitem__(self, index):
+            """
+            Args:
+                index (int): Index
+            Returns:
+                tuple: (image, y) where y is the label of the image.
+            """
+            img = None
+            with open(os.path.join(self.img_path, self.imgs[index]), 'rb') as f:
+                img = Image.open(f)
+                img = img.convert('RGB')
+                if self.transform is not None:
+                    img = self.transform(img)
+            y = self.classidx[index]
+            return img, y
+
+    def __len__(self):
+        return len(self.imgs)
+
+data_dir = 'tiny-imagenet-200/'
+
+num_workers = {'train' : 10,'val'   : 0,'test'  : 0}
+
+traindir = os.path.join(data_dir, 'train')
+valdir = os.path.join(data_dir, 'val')
+train_batch = 64
+test_batch = 200
+
+
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
+
+train_dataset = datasets.ImageFolder(traindir, transforms.Compose([
+            transforms.Resize(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ]))
+    
+train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=train_batch, shuffle=True,
+        num_workers=num_workers['train'], pin_memory=True)
+    
+valdir = os.path.join(data_dir, 'val', 'images')
+valgtfile = os.path.join(data_dir, 'val', 'val_annotations.txt')
+val_dataset = TImgNetDataset(valdir, valgtfile, class_to_idx=train_loader.dataset.class_to_idx.copy(),
+            transform=transforms.Compose([
+            transforms.Resize(224),
+            transforms.ToTensor(),
+            normalize,
+            ]))
+val_loader = torch.utils.data.DataLoader(
+        val_dataset,
+        batch_size=test_batch, shuffle=False,
+        num_workers=num_workers['val'], pin_memory=True)
+
+import argparse
+
+parser = argparse.ArgumentParser(
+    description='Pytorch Imagenet Training')
+parser.add_argument('--config', default='/home/fenglei/configs/config_regular_c109_n32.yaml')
+parser.add_argument("--local_rank", type=int)
+parser.add_argument(
+    '--port', default=29500, type=int, help='port of server')
+parser.add_argument('--world-size', default=1, type=int)
+parser.add_argument('--rank', default=0, type=int)
+parser.add_argument('--model_dir', type=str)
+parser.add_argument('--resume', default=False, help='resume')
+parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
+                    help='evaluate model on validation set')
+
+
+args = parser.parse_args()
+
+with open(args.config) as f:
+        config = yaml.load(f)
+
+for key in config:
+        for k, v in config[key].items():
+            setattr(args, k, v)
+
+dataset_sizes = {'train': len(train_dataset), 'val': len(val_dataset)}
+
+dataloaders = {'train': train_loader, 'val': val_loader}
+
+model_ft = CNN(args)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model_ft = model_ft.to(device)
+
+print(sum(p.numel() for p in model_ft.parameters() if p.requires_grad))
+
+#Loss Function
+criterion = nn.CrossEntropyLoss()
+# Observe that all parameters are being optimized
+Init_lr = 0.01
+optimizer_ft = optim.SGD(model_ft.parameters(), lr=Init_lr, momentum=0.9)
+
+
+def adjust_learning_rate(optimizer, epoch, Init_lr):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    lr = Init_lr * (0.1 ** (epoch // 30))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+    print(param_group['lr'])
+
+
+
+def train_model(output_path, model, dataloaders, dataset_sizes, criterion, optimizer, num_epochs=100, scheduler=None):
+    if not os.path.exists('models/'+str(output_path)):
+        os.makedirs('models/'+str(output_path))
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+  
+    since = time.time()
+    
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+    best = 0
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch+1, num_epochs))
+        print('-' * 10)
+        
+        adjust_learning_rate(optimizer_ft, epoch, Init_lr)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                if scheduler != None:
+                    scheduler.step()
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for i,(inputs, labels) in enumerate(dataloaders[phase]):
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+                print("\rIteration: {}/{}, Loss: {}.".format(i+1, len(dataloaders[phase]), loss.item() * inputs.size(0)), end="")
+
+#                 print( (i+1)*100. / len(dataloaders[phase]), "% Complete" )
+                sys.stdout.flush()
+                
+                
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+            if phase == 'train':
+                avg_loss = epoch_loss
+                t_acc = epoch_acc
+            else:
+                val_loss = epoch_loss
+                val_acc = epoch_acc
+            
+#             print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+#                 phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best = epoch + 1
+                best_model_wts = copy.deepcopy(model.state_dict())
+                
+
+        print('Train Loss: {:.4f} Acc: {:.4f}'.format(avg_loss, t_acc))
+        print(  'Val Loss: {:.4f} Acc: {:.4f}'.format(val_loss, val_acc))
+        print()
+        torch.save(model.state_dict(), './models/' + str(output_path) + '/model_{}_epoch.pt'.format(epoch+1))
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best Validation Accuracy: {}, Epoch: {}'.format(best_acc, best))
+    
+
+def test_model(model, dataloaders, dataset_sizes, criterion, optimizer):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    since = time.time()
+
+    # Each epoch has a training and validation phase
+    for phase in ['test']:
+        model.eval()   # Set model to evaluate mode
+
+        running_loss = 0.0
+        running_corrects = 0
+
+        # Iterate over data.
+        for i,(inputs, labels) in enumerate(dataloaders[phase]):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward
+            # track history if only in train
+            with torch.set_grad_enabled(False):
+                outputs = model(inputs)
+                _, preds = torch.max(outputs, 1)
+                loss = criterion(outputs, labels)
+
+            # statistics
+            running_loss += loss.item() * inputs.size(0)
+            running_corrects += torch.sum(preds == labels.data)
+            print("\rIteration: {}/{}, Loss: {}.".format(i+1, len(dataloaders[phase]), loss.item() * inputs.size(0)), end="")
+            sys.stdout.flush()
+
+
+        epoch_loss = running_loss / dataset_sizes[phase]
+        epoch_acc = running_corrects.double() / dataset_sizes[phase]
+    print()
+    print('Test Loss: {:.4f} Acc: {:.4f}'.format(epoch_loss, epoch_acc))
+    print()
+    
+    time_elapsed = time.time() - since
+    print('Test complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))   
+
+    
+train_model("fenglei/Alex",model_ft, dataloaders, dataset_sizes, criterion, optimizer_ft, num_epochs=90)
+
+test_model(model_ft, dataloaders, dataset_sizes, criterion, optimizer_ft)
+
+
